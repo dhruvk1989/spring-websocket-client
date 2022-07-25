477291fc8906dadfc4db7990c9d3fde8a132ee5a
dhruvk1989 <dhruvachintya22@gmail.com>
2022-07-25 12:52:43 +0530

    First commit of creation of a spring boot websocket client

---
+++ src/main/java/com/dhruv/springwebsocketclient/SpringWebsocketClientApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.springwebsocketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruv/springwebsocketclient/SpringWebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.springwebsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
